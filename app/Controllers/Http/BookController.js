9b91d888a332f3fd18ccbe6a856e9e51ae0810e6
Robert Guss <robertguss@gmail.com>
2020-04-15 08:28:55 -0400

    finished section

--- app/Controllers/Http/BookController.js
+++ app/Controllers/Http/BookController.js
@@ -41,6 +41,47 @@ class BookController {
     session.flash({ notification: "Book Created" });
     return response.redirect("/");
   }
+
+  async show({ params, view }) {
+    const book = await Book.find(params.id);
+
+    return view.render("book.show", {
+      book,
+    });
+  }
+
+  async edit({ params, view }) {
+    const book = await Book.find(params.id);
+
+    return view.render("book.edit", {
+      book,
+    });
+  }
+
+  async update({ params, request, response, session }) {
+    const book = await Book.find(params.id);
+
+    book.title = request.input("title");
+    book.author = request.input("author");
+    book.cover_image = request.input("cover_image");
+    book.isbn = request.input("isbn");
+
+    await book.save();
+
+    session.flash({ notification: "Book Updated" });
+
+    return response.redirect("/");
+  }
+
+  async destroy({ params, session, response }) {
+    const book = await Book.find(params.id);
+
+    await book.delete();
+
+    session.flash({ notification: "Book Deleted" });
+
+    return response.redirect("/");
+  }
 }
 
 module.exports = BookController;
--- start/routes.js
+++ start/routes.js
@@ -20,3 +20,7 @@ const Route = use("Route");
 Route.get("/", "BookController.index");
 Route.get("books/create", "BookController.create");
 Route.post("books", "BookController.store");
+Route.get("books/:id", "BookController.show");
+Route.get("/books/:id/edit", "BookController.edit");
+Route.put("/books/:id", "BookController.update");
+Route.delete("/books/:id", "BookController.destroy");
